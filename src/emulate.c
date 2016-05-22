43ab9bff98695e7925591d520456288b715fd319
mk6515 <mk6515@ic.ac.uk>
2016-05-22 11:18:54 +0100

    Implemented printing out of results - not complete. Need to refine hexadecimal representation of registers, memory and memory addresses.

--- src/emulate.c
+++ src/emulate.c
@@ -2,5 +2,23 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+
+
+
+	printf("Registers:\n");
+	for(int i = 0; i < 13; i++) {
+		printf("$%i  :          %d (0x%x)\n", i, arm->registers[i]); 
+	}
+	printf("PC  :          %d (0x%x)\n", arm->registers[15]);
+	printf("CPSR:          %d (0x%x)\n", arm->registers[16]);
+	printf("Non-zero memory:\n);
+
+	int i = 0;
+
+	while(arm->memory[i*4] != 0) {
+		printf("0x%i: 0x%x\n", i*4, arm->memory[i*4]);
+		i++;	
+	}
+	
   return EXIT_SUCCESS;
 }
