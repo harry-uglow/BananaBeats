691a332235aaecbff30cead805e68ad7964d2463
Mohammad Khaksar-Ghalati <mk6515@corona22.doc.ic.ac.uk>
2016-05-23 13:06:02 +0100

    Updated implementation of reading from file input

--- src/emulate.c
+++ src/emulate.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "defs.h"
 
+
+
 void printFinalState(arm_t state);
 
 
@@ -10,11 +12,18 @@ int main(int argc, char **argv) {
 	arm_t arm;
 	
 	// Reading file input for the program	
-	FILE *fp = fopen(argv[1],"r");
+	FILE *finput = fopen(argv[1],"rb");
 
-		// Code to be written
+	if(finput == NULL) {
+	    printf("Could not open %s", argv[1]);
+	    return 0;
+	}
+	
+	while(fscanf(finput, "%i", (int *) arm.memory) != EOF) {
+	    continue;
+	}
 
-	fclose(fp);
+	fclose(finput);
 	// Finished reading file input	
 	
 
@@ -22,14 +31,10 @@ int main(int argc, char **argv) {
 
 	
 
-	
+	 return EXIT_SUCCESS;
 
-	
-
-
-	
-  return EXIT_SUCCESS;
 }
+
 
 
 
@@ -39,20 +44,23 @@ void printFinalState(arm_t state) {
 	
 	printf("Registers:\n");
 
-        int pc   = state.registers[REG_PC];
-        int cpsr = state.registers[REG_CPSR];
+    int pc   = state.registers[REG_PC];
+    int cpsr = state.registers[REG_CPSR];
 
-        for(int i = 0; i < 13; i++) {
-                printf("$%i  :          %d (0x%x)\n", i, state.registers[i], (unsigned int)  state.registers[i]); 
-        }
-        printf("PC  :          %d (0x%08x)\n", pc, (unsigned int) pc);
-        printf("CPSR:          %d (0x%08x)\n", cpsr, (unsigned int) cpsr);
-        printf("Non-zero memory:\n");
+    for(int i = 0; i < 13; i++) {
+    	printf("$%i  :          %d (0x%x)\n", i, state.registers[i], (unsigned int)  state.registers[i]); 
+    }
+    printf("PC  :          %d (0x%08x)\n", pc, (unsigned int) pc);
+    printf("CPSR:          %d (0x%08x)\n", cpsr, (unsigned int) cpsr);
+    printf("Non-zero memory:\n");
 
-        int j = 0;
+    int j = 0;
 
-        while(state.memory[j*4] != 0) {
-                printf("0x%08x: 0x%08x\n", j*4, (state.memory[j*4]));
-                j++;
-        }
+	while(state.memory[j*4] != 0) {
+    	printf("0x%08x: 0x%08x\n", j*4, (state.memory[j*4]));
+        j++;
+	}
+
 }
+
+
