d712546d3db4e05d6e5d62a363436a422b61c509
Mohammad Khaksar-Ghalati <mk6515@graphic17.doc.ic.ac.uk>
2016-05-24 15:22:24 +0100

    Extracted out main helper programs into utils and fixing include directives

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,9 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "utils.h"
+
+
+#define TRUE 1
 
 int32_t executeShift(int32_t value, int shiftType, int amount) {
     int32_t shiftedValue = 0;
@@ -65,3 +69,104 @@ int checkCond(int cond, int NZCV){
     }
     return passesCond;
 }
+
+
+void printFinalState(arm_t *state) {
+
+        printf("Registers:\n");
+
+    int pc   = state->registers[REG_PC];
+    int cpsr = state->registers[REG_CPSR];
+
+        // Print out contents of registers 0-9
+    for(int i = 0; i < 10; i++) {
+        printf("$%i  :%12d (0x%08x)\n", i, state->registers[i], state->registers[i]);
+    }
+
+        // Print out contents of registers 10-12
+    for(int j = 0; j < 13; j++) {
+                printf("$%i :%12d (0x%08x)\n", j, state->registers[j], state->registers[j]);
+        }
+
+        // Print out contents of PC and CPSR
+    printf("PC  :%12d (0x%08x)\n", pc, pc);
+    printf("CPSR:%12d (0x%08x)\n", cpsr, cpsr);
+
+        // Print out non-zero contents of memory
+        printf("Non-zero memory:\n");
+
+    int k = 0;
+
+        while(state->memory[k*4] != 0) {
+        printf("0x%08x: 0x%08x\n", k*4, (state->memory[k*4]));
+        k++;
+        }
+
+}
+
+int iteratePipeline(arm_t *state) {
+        if(state->isDecoded) {
+        if(state->instruction->type == HALT) {
+                        return 0;
+                }
+                execute(&state);
+        }
+
+        if(state->isFetched) {
+                decode(&state);
+        }
+        fetch(&state);
+        return 1;
+}
+
+void readFile(arm_t *state, char **argv) {
+
+        // Reading file input   
+        FILE *finput = fopen(argv[1],"rb");
+
+        if(finput == NULL) {
+            printf("Could not open %s", argv[1]);
+            return 0;
+        }
+
+        // Loop to read binary file, one byte at a time,
+        // and copy the bytes into processor's memory until
+        // there are no more bytes left to read in the file
+        int8_t byteInput; // Temporary byte variable to store read byte on each iteration of the loop
+        int8_t *pByteInput = &byteInput;
+        int memPos = 0;
+        while(TRUE) {
+                int in = fread(pByteInput,sizeof(int8_t),1,finput);
+                if(in != 1) {
+                        break;
+                }
+                state->memory[memPos] = byteInput;
+                memPos++;
+        }
+        fclose(finput);
+        // Finished reading file input
+
+}
+
+void initialiseProcessor(arm_t *state) {
+
+        // Assign memory array onto heap
+    int8_t *memory = calloc(MEM_SIZE, sizeof(int8_t));
+    if (memory == NULL) {
+        printf("Failed to create memory array on heap");
+        return 0;
+    }
+
+    // Assign registers array onto heap
+    int32_t *registers = calloc(NUMBER_OF_REGISTERS, sizeof(int32_t));
+    if (registers == NULL) {
+        printf("Failed to create registers array on heap");
+        return 0;
+    }
+
+    // Point the arm state to register and memory arrays on the heap
+    state->memory = memory;
+    state->registers = registers;
+
+}
+             
--- src/utils.h
+++ src/utils.h
@@ -2,8 +2,15 @@
 #define ARM11_19_UTILS_H
 
 #include <stdint.h>
+#include "defs.h"
+#include "pipeline.h"
+
 
 int32_t executeShift(int32_t value, int shiftType, int amount);
 int checkCond(int cond, int NZCV);
+void initialiseProcessor(arm_t *state);
+void readFile(arm_t *state, char **argv);
+int iteratePipeline(arm_t *state);
+void printFinalState(arm_t *state);
 
 #endif //ARM11_19_UTILS_H
