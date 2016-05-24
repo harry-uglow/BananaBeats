e37495b933b54ae364f04b26b0649290cf84c18c
mk6515 <mk6515@ic.ac.uk>
2016-05-24 23:18:06 +0100

    Added reverseByteOrder to utils.h

--- src/utils.h
+++ src/utils.h
@@ -6,5 +6,5 @@ int checkCond(int cond, int NZCV);
 int initialiseProcessor(arm_t *state);
 int readFile(arm_t *state, char **argv);
 void printFinalState(arm_t *state);
-
+int32_t reverseByteOrder(int32_t n);
 #endif //ARM11_19_UTILS_H
