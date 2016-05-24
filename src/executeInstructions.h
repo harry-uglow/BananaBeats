51d6ec7da2ac58c97ec7ba1b7f9ca1baf0fca1b3
jsc115 <jsc115@imperial.ac.uk>
2016-05-24 14:45:55 +0100

    Had to readjust the executeInstructions.h to include the data processing function

--- src/executeInstructions.h
+++ src/executeInstructions.h
@@ -1,12 +1,8 @@
 #ifndef ARM11_19_EXECUTEINSTRUCTIONS_H
 #define ARM11_19_EXECUTEINSTRUCTIONS_H
-
-<<<<<<< HEAD
-void dataProcessing(arm_t *arm);
-=======
 #include "defs.h"
 
->>>>>>> 768e75eb2c57390d3736a2008bd2b9886eef6ce4
+void dataProcessing(arm_t *arm);
 void multiply(arm_t *arm);
 void branch(arm_t *arm);
 void singleDataTransfer(arm_t *arm);
