142bd8a495ff1cbc270e195818b94dcfd6376f09
Mohammad Khaksar-Ghalati <mk6515@voxel05.doc.ic.ac.uk>
2016-05-26 17:46:43 +0100

    Implemented utils.c and utils.h
    Added initial framework for readFile and began work on initialiseAssembler which will be used to allocate memory onto the heap for the array of instructions.

---
+++ src/assembler/utils.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int readFile(assIns_t *instructions, char **argv) {
+	// Reading assembly file input
+	FILE *finput = fopen(argv[1],"r");
+	
+	// Check if assembly file cannot be opened
+	if (finput == NULL) {
+		printf("Could not open %s\n", argv[1]);
+		return 0;
+	}
+	
+	// TODO: code for reading strings of each assembly instruction
+
+	// Finished reading input of assembly file
+	fclose(finput);
+	return 1;
+}
+
+int initialiseAssembler(void) {
+	assIns_t *instructions = malloc(sizeof(assIns_t) * MEM_SIZE);
+	
+	// Check if memory allocation failed
+	if(instructions == NULL) {
+		printf("Memory could not be allocated onto the heap.\n");
+		return 0;
+	}
+ 	
+	// TODO: Finish complete initialisation of assembler 
+}
--- src/assembler/utils.h
+++ src/assembler/utils.h
@@ -1,6 +1,6 @@
 #ifndef ARM11_19_ASSEMBLER_UTILS_H
 #define ARM11_19_ASSEMBLER_UTILS_H
 
-void readFile(assIns_t *instructions[], char **argv);
+int readFile(assIns_t *instructions, char **argv);
 
 #endif //ARM11_19_ASSEMBLER_UTILS_H
