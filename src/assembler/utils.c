541b16bd0027b90820ab8d4d6be9f2ed4df1a171
mk6515 <mk6515@ic.ac.uk>
2016-05-29 21:07:45 +0100

    Implemented writeToBinaryFile - not tested yet

--- src/assembler/utils.c
+++ src/assembler/utils.c
@@ -4,7 +4,7 @@
 #include "defs.h"
 
 int readFile(assIns_t *instructions, char **argv) {
-	// Open file to read frmo
+	// Open file to read from
 	FILE *finput = fopen(argv[1],"r");
 	
 	// Check if assembly file cannot be opened
@@ -12,7 +12,8 @@ int readFile(assIns_t *instructions, char **argv) {
 		printf("Could not open file %s\n", argv[1]);
 		return 0;
 	}
-	
+
+
 	// TODO: code for reading strings of each assembly instruction
 
 	// Finished reading input of assembly file
@@ -28,16 +29,13 @@ int initialiseAssembler(assIns_t *instructions) {
 	if(instructions == NULL) {
 		printf("Memory could not be allocated onto the heap.\n");
 		return 0;
-	}
- 	
-	// TODO: Finish complete initialisation of assembler 
-	
+	}	
 	return 1;
 }
 
 int writeToBinaryFile(int32_t *binInstructions, char **argv) {
 	// Open file to write to
-	FILE *foutput = fopen(argv[2], "w");
+	FILE *foutput = fopen(argv[2], "wb");
 
 	// Check if file cannot be opened
 	if(foutput == NULL) {
@@ -45,7 +43,10 @@ int writeToBinaryFile(int32_t *binInstructions, char **argv) {
 		return 0;
 	}
 	
-	// TODO: Complete writing to binary file
+	// Write all of the binary instructions encoded into the output file
+	while(fwrite(binInstructions, sizeof(int32_t), 1, foutput) == 1) {
+		binInstructions++;
+	}
 
 	// Finished writing to output of binary file
 	fclose(foutput); 
