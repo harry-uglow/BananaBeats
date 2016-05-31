686f992a5fe1f3538ecfe5e2df267d577c422e4f
datglowtho <harryu@live.co.uk>
2016-05-31 12:52:59 +0100

    firstPass() now appears to be working.

--- src/assembler/utils.c
+++ src/assembler/utils.c
@@ -13,12 +13,11 @@ int firstPass(char **argv) {
 		return 0;
 	}
 
-	char buffer[MAX_LINE_LENGTH];
+	char *buffer = calloc(MAX_LINE_LENGTH, sizeof(char));
 	char *token;
-	char *ops[4];
+	char *ops = calloc(MAX_OPERAND_LENGTH, sizeof(char));
 	char *rest;
-	char *temp[4];
-	int opSignal[4] = {0, 0, 0, 0};
+	char *temp = calloc(MAX_OPERAND_LENGTH, sizeof(char));
 
 	while(fgets(buffer, MAX_LINE_LENGTH, finput) != NULL) {		
 		// Remove '\n' character at the end of the string
@@ -31,57 +30,65 @@ int firstPass(char **argv) {
         if(isLabel(token)) {
             SymbolTable_put(token, &address, &table);
         } else {
-            instruction[address].mnemonic = token;
+            strcpy(instruction[address].mnemonic, token);
 		}
 	
 		// Parse through rest of the string & fill up array of operands
-		for(int i = 0; i < 4; i++) {
-			if(checkBrackets(rest, temp[i])) {
-				ops[i] = strtok(rest, TOK_DELIM);
-				opSignal[i] = 1;
-				strcpy(buffer, rest);	
-			} else {	
-				ops[i] = strtok(NULL, TOK_DELIM);
-			}		
+		for(int i = 1; i <= 4; i++) {
+            if(!rest) {
+                break;
+            }
+			if(rest[0] == '[') {
+                int j = 0;
+                while(rest[0] != ']') {
+                    temp[j] = rest[0];
+                    rest++;
+                    j++;
+                }
+                temp[j] = rest[0];
+                temp[j + 1] = '\0';
+                strcpy(ops, temp);
+                rest++;
+            } else {
+				strcpy(ops, strtok(rest, TOK_DELIM));
+                rest = strtok(NULL, "");
+			}
+            if(ops != NULL) {
+                switch (i) {
+                    case 1 :
+                        strcpy(instruction[address].op1, ops);
+                        break;
+                    case 2 :
+                        strcpy(instruction[address].op2, ops);
+                        break;
+                    case 3 :
+                        strcpy(instruction[address].op3, ops);
+                        break;
+                    case 4 :
+                        strcpy(instruction[address].op4, ops);
+                        break;
+                    default:
+                        break;
+                }
+            }
 		}
 	
-		char *strToken;
-		// Set all non-null operands to compoents of assembly instructions
+		/*// Set all non-null operands to components of assembly instructions
 		if(ops[0] != NULL) {
-			if(opSignal[0]) {
-				strToken = temp[0];
-			} else {
-				strToken = ops[0];
-			}
-			instruction[address].op1 = strToken;
+            strcpy(instruction[address].op1, ops[0]);
 		}
 
-		if(ops[1] != NULL) {
-			if(opSignal[1]) {
-				strToken = temp[1];
-			} else {
-				strToken = ops[1];
-			}
-			instruction[address].op2 = strToken;
-		}
-	
-		if(ops[2] != NULL) {
-			if(opSignal[2]) {
-				strToken = temp[2];
-			} else {
-				strToken = ops[2];
-			}
-			instruction[address].op3 = strToken;
-		}
+        if(ops[1] != NULL) {
+            strcpy(instruction[address].op1, ops[1]);
+        }
 
-		if(ops[3] != NULL) {
-			if(opSignal[3]) {
-				strToken = temp[3];
-			} else {
-				strToken = ops[3];
-			}
-			instruction[address].op4 = strToken;
-		}	
+        if(ops[2] != NULL) {
+            strcpy(instruction[address].op1, ops[2]);
+        }
+
+        if(ops[3] != NULL) {
+            strcpy(instruction[address].op1, ops[3]);
+        }*/
 		
 		// Increment address
 		address++;
@@ -96,21 +103,23 @@ int initialiseAssembler() {
 	// Allocate memory onto the heap for an array of instructions
 	instruction = calloc(MEM_SIZE, sizeof(assIns_t));
 
-    for(int i = 0; i < (sizeof(*instruction) / sizeof(instruction[0])); i++) {
+    // Check if memory allocation failed
+    if(instruction == NULL) {
+        printf("Memory could not be allocated onto the heap.\n");
+        return 0;
+    }
+
+    for(int i = 0; i < MEM_SIZE; i++) {
         instruction[i].mnemonic = calloc(MAX_MNEMONIC_LENGTH, sizeof(char));
         instruction[i].op1 = calloc(MAX_OPERAND_LENGTH, sizeof(char));
         instruction[i].op2 = calloc(MAX_OPERAND_LENGTH, sizeof(char));
         instruction[i].op3 = calloc(MAX_OPERAND_LENGTH, sizeof(char));
         instruction[i].op4 = calloc(MAX_OPERAND_LENGTH, sizeof(char));
     }
-	
-	// Check if memory allocation failed
-	if(instruction == NULL) {
-		printf("Memory could not be allocated onto the heap.\n");
-		return 0;
-	}	
 
-	// Create new symbol table
+    memory = calloc(MEM_SIZE, sizeof(int8_t));
+
+    // Create new symbol table
 	table = *SymbolTable_new();
 
 	// Initialise address counter
@@ -150,11 +159,11 @@ int isLabel(char *token) {
 void secondPass() {
 	for(int i = 0; i < (sizeof(*instruction) / sizeof(instruction[0])); i++) {
         assIns_t assIns = instruction[i];
-		int32_t instruction = encode(&assIns);
-		memory[WORD_LENGTH * i] = (int8_t)(MASK_BYTE_0 & instruction);
-        memory[(WORD_LENGTH * i) + 1] = (int8_t)(MASK_BYTE_1 & instruction);
-        memory[(WORD_LENGTH * i) + 2] = (int8_t)(MASK_BYTE_2 & instruction);
-        memory[(WORD_LENGTH * i) + 3] = (int8_t)(MASK_BYTE_3 & instruction);
+		int32_t ins = encode(&assIns);
+		memory[WORD_LENGTH * i] = (int8_t)(MASK_BYTE_0 & ins);
+        memory[(WORD_LENGTH * i) + 1] = (int8_t)(MASK_BYTE_1 & ins);
+        memory[(WORD_LENGTH * i) + 2] = (int8_t)(MASK_BYTE_2 & ins);
+        memory[(WORD_LENGTH * i) + 3] = (int8_t)(MASK_BYTE_3 & ins);
 	}
 }
 
@@ -162,17 +171,3 @@ void removeNewline(char *token) {
 	token[strlen(token) - 1] = '\0';
 }
 
-int checkBrackets(char *token, char *temp) {
-	if(token[0] == '[') {
-		int i = 0;
-		while(token[0] != ']') {
-			temp[i] = token[0];
-			token++;
-			i++; 
-		}
-		temp[i] = token[0];
-		token++;
-		return 1;
-	} 
-	return 0;
-}
