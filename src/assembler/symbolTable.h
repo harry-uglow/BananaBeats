ca4501e65d5f4718b7149523a84dded5f7881373
Mohammad Khaksar-Ghalati <mk6515@voxel01.doc.ic.ac.uk>
2016-05-31 12:58:09 +0100

    Rename symbolTable to fix make problem and removed Werror for now to test our cases

---
+++ src/assembler/symbolTable.h
@@ -0,0 +1,28 @@
+#ifndef ARM11_19_SYMBOLTABLE_H
+#define ARM11_19_SYMBOLTABLE_H
+
+typedef struct SymbolTableNode {
+    char *label; // 'Key' of the symbol table, the label
+    void *value; // 'Value' of the symbol table. Pointer to a value
+
+    struct SymbolTableNode *pNextNode; // The next node (node linked to)
+} symbolTableNode_t;
+
+typedef struct SymbolTable {
+    symbolTableNode_t *pFirstNode;
+} symbolTable_t;
+
+// Create new symbol table
+symbolTable_t *SymbolTable_new(void);
+
+// Remove the symbol table from the heap (i.e. garbage collection)
+void SymbolTable_delete(symbolTable_t *symbolTable);
+
+// Add matching label and value to symbol table
+void SymbolTable_put(char *newLabel, void *newValue,
+                     symbolTable_t *symbolTable);
+
+// Find value from given label
+void *SymbolTable_get(char *key, symbolTable_t *symbolTable);
+
+#endif //ARM11_19_SYMBOLTABLE_H
