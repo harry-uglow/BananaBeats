c41f00053e60605626cab687c635c1fbe0ce2805
Maurice Yap <mauriceyap@hotmail.co.uk>
2016-06-02 01:52:28 +0100

    Removed uneccessary labels from gpio.s and changed output pin to 16 instead of 7

--- gpio.s
+++ gpio.s
@@ -1,36 +1,26 @@
-.section .init 
-.globl _start 
-_start:
     ldr r0, =0x20200000   /* load base for GPIO into r0 */
     mov r1, #1            /* load 1 into r1 */
-    lsl r1, #21           /* put bit in correct place to mask pin 7 */
-    str r1, [r0]          /* store value of r1 in memory to activate pin 7 */
-    mov r2, #1            /* load 1 into r2 */
-    lsl r2, #7            /* create mask to clear and set pin 7 */
-
+    lsl r1, #18           /* put bit in correct place to mask GPIO 16 */
+    str r1, [r0,#4]       /* store value of r1 in GPIO 16's memory */
+    mov r1, #1            /* load 1 into r1 */
+    lsl r1, #7            /* create mask to clear and set GPIO 16 */
 Loop:
    str r2, [r0, #40]      /* clear pin 7 */
    b DelayCLR
 FinCLRDelay:
-
    str r2, [r0, #28]      /* set pin 7 */
    b DelaySET
 FinSETDelay:
-
    b Loop
-
-
-
 DelayCLR:
-    mov r4, 1000
+    mov r4, 10
 DelayLoopCLR:
     sub r4, #1
     cmp r4, 0
     bgt DelayLoopCLR
     b FinCLRDelay
-    
 DelaySET:
-    mov r4, 1000
+    mov r4, 10
 DelayLoopSET:
     sub r4, #1
     cmp r4, 0
