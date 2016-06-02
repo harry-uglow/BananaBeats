9181a678e57fb24e4c69e5edf6c2f31457aa4e2c
Maurice Yap <mauriceyap@hotmail.co.uk>
2016-06-02 03:41:33 +0100

    I made something, it works and I don't know why

--- gpio.s
+++ gpio.s
@@ -1,28 +1,28 @@
     ldr r0, =0x20200000   /* load base for GPIO into r0 */
     mov r1, #1            /* load 1 into r1 */
     lsl r1, #18           /* put bit in correct place to mask GPIO 16 */
-    str r1, [r0,#4]       /* store value of r1 in GPIO 16's memory */
-    mov r1, #1            /* load 1 into r1 */
-    lsl r1, #7            /* create mask to clear and set GPIO 16 */
-Loop:
-   str r2, [r0, #40]      /* clear pin 7 */
-   b DelayCLR
-FinCLRDelay:
-   str r2, [r0, #28]      /* set pin 7 */
-   b DelaySET
-FinSETDelay:
-   b Loop
-DelayCLR:
-    mov r4, 10
-DelayLoopCLR:
-    sub r4, #1
-    cmp r4, 0
-    bgt DelayLoopCLR
-    b FinCLRDelay
-DelaySET:
-    mov r4, 10
-DelayLoopSET:
-    sub r4, #1
-    cmp r4, 0
-    bgt DelayLoopSET
-    b FinSETDelay
+    str r1, [r0,#4]       /* store value of r1 in GPIO 16's indicator */
+    mov r2, #1            /* load 1 into r2 */
+    lsl r2, #16            /* mask to clear and set GPIO 16 (set bit 16) */
+loop:
+   str r2, [r0, #40]      /* clear GPIO 16 */
+   b delayCLR
+finCLRDelay:
+   str r2, [r0, #28]      /* set GPIO 16 */
+   b delaySET
+finSETDelay:
+   b loop
+delayCLR:
+    mov r4, #1000
+delayLoopCLR:
+    sub r4, r4, #1
+    cmp r4, #0
+    bgt delayLoopCLR
+    b finCLRDelay
+delaySET:
+    mov r4, #1
+delayLoopSET:
+    sub r4, r4, #1
+    cmp r4, #0
+    bgt delayLoopSET
+    b finSETDelay
