b831913d36d4715f2be4b414dff3a0cbdd93a2c9
Mohammad Khaksar-Ghalati <mk6515@voxel04.doc.ic.ac.uk>
2016-06-06 14:18:59 +0100

    Basic framework for C function calling python loop implemented, needs to be corrected/completed once we figure out the exact workings of python extension programming

--- programs/part_4/main.c
+++ programs/part_4/main.c
@@ -1,8 +1,13 @@
 #include <Python.h>
+#define TRUE 1
 
 int main() {
 
-    
+	while(TRUE) {
+		// TODO: Correct call of python function loopIteration
+		// This is just a basic framework for the C function 
+		loopIteration();
+	}    
 
     return 0;
 }
