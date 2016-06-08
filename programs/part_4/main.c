37eb03af007d8c9c155622dd1534c21ae30848bd
Maurice Yap <mauriceyap@hotmail.co.uk>
2016-06-08 15:40:30 +0000

    Added sound folders

--- programs/part_4/main.c
+++
@@ -1,9 +0,0 @@
-#include <Python.h>
-
-static PyObject *loop(PyObject *self) {
-	// TODO: Complete correct C function call to be called from run.py
-	// Since this returns a pointer to a PyObject, we can't have a loop
-	// as originally planned		 
-	loopIteration();
-}
-
