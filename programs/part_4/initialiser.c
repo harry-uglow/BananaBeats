28cf77a19f78662b5a3826994b1c3ce643e7dc1c
Maurice Yap <mauriceyap@hotmail.co.uk>
2016-06-09 01:36:13 +0100

    Sound filepaths strings built and added to symbol tables inside the array. More code correction

--- programs/part_4/initialiser.c
+++ programs/part_4/initialiser.c
@@ -1,27 +1,49 @@
 #include <stdio.h>
+#include <string.h>
 #include "utils.h" 
 #define NUM_OF_INSTRUMENTS 3
 #define NUM_OF_SOUNDFILES 12
+#define MAX_DIGITS_NUMBER_OF_MODES 15
+#define MAX_DIGITS_NUMBER_OF_SOUNDS 3
+#define MAX_LENGTH_FILEPATH_STRING 100
 
-// Symbol table array as global variable
+// Array of symbol tables as global variable
 symbolTable_t *set[NUM_OF_INSTRUMENTS];
 
+// Initialise array of symbol tables for each sound set (instrument)
 static void initialiseSymbolTables(void) {
-	// Initialise array of symbol tables for each sound set (instrument) 
 	for(int i = 0; i < NUM_OF_INSTRUMENTS; i++) {
 		set[i] = SymbolTable_new();
 	}
-
+    
+    char* filePathToFolder = "sounds/set";
+    char modeNumber[MAX_DIGITS_NUMBER_OF_MODES];
+    char soundNumber[MAX_DIGITS_NUMBER_OF_SOUNDS];
+    char* fileExtension = ".wav";
+    char filePathToSound[MAX_LENGTH_FILEPATH_STRING];
 	// Insert pin-sound entries into each symbol table for each instrument
-	for(int j = 0; j < NUM_OF_INSTRUMENTS; j++) {
- 		
-		for(int k = 0; j < NUM_OF_SOUNDFILES; j++) {
+	for(int j = 0; j < NUM_OF_INSTRUMENTS; j++) { // For each sound mode
+        sprintf(modeNumber, "%d/", j);
+		for(int k = 0; k < NUM_OF_SOUNDFILES; k++) {
+            sprintf(soundNumber, "%d", k);
+            strcpy(filePathToSound, filePathToFolder);
+            strcat(filePathToSound, modeNumber);
+            strcat(filePathToSound, soundNumber);
+            strcat(filePathToSound, fileExtension);
+            SymbolTable_put(&k, filePathToSound, set[j]);
 		} 
 	}
 }
+
 
 static void freeSymbolTables(void) {
 	for(int i = 0; i < NUM_OF_INSTRUMENTS; i++) {
 		SymbolTable_delete(set[i]);
 	}
+}
+
+int main() {
+    initialiseSymbolTables();
+    freeSymbolTables();
+    return 0;
 }
