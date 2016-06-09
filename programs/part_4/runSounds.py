28a1298b6da3a432441522cbb79411824318334d
Maurice Yap <mauriceyap@hotmail.co.uk>
2016-06-09 19:20:00 +0000

    Multi-threading almost there

--- programs/part_4/runSounds.py
+++ programs/part_4/runSounds.py
@@ -1,25 +1,27 @@
 from touchHat import *
-from threading import Thread
+import threading
 import sys
 import time
 import pygame
 
 import python.adafruit_libraries.Adafruit_MPR121.MPR121 as MPR121
 
-initSounds()
-t = Thread(target=gui)
-t.start()
 
-# Create MPR121 instance
-device = MPR121.MPR121()
-
-if not device.begin():
-    sys.exit(1)
-
-pygame.mixer.pre_init(44100, -16, 12, 512)
-pygame.init()
-
-sounds = [0,0,0,0,0,0,0,0,0,0,0,0]
+def runSounds():
+    # Main loop to play the corroect sound every time a pin is touched
+    last_touched = device.touched()
+    while True:
+	updateSoundArray(getInstrument(), getVolume())
+	current_touched = device.touched()
+	for i in range(12):
+	    pin_bit = 1 << i
+	    if current_touched & pin_bit and not last_touched & pin_bit:
+		sounds[i].play()
+		touched(i)
+	    if not current_touched & pin_bit and last_touched & pin_bit:
+		released(i)
+	last_touched = current_touched
+	time.sleep(0.01) 
 
 def updateSoundArray(instrument, volume):
     if not updateSoundArray.lastIns == instrument:
@@ -29,21 +31,33 @@ def updateSoundArray(instrument, volume):
             sounds[pin] = pygame.mixer.Sound(soundfile)
             sounds[pin].set_volume(volume)
 
-updateSoundArray.lastIns = -1
+if __name__ == '__main__':
+    t = threading.Thread(target=gui, args=())
+    t.start()
+    print 'boobs'
+    time.sleep(4)
+    print 'boobs'
+    initSounds()
+    # Create MPR121 instance
+    print 'boobs'
+    device = MPR121.MPR121()
 
-print "yep"
-sys.stdout.flush()
-# Main loop to play the corroect sound every time a pin is touched
-last_touched = device.touched()
-while True:
-    updateSoundArray(getInstrument(), getVolume())
-    current_touched = device.touched()
-    for i in range(12):
-        pin_bit = 1 << i
-        if current_touched & pin_bit and not last_touched & pin_bit:
-            sounds[i].play()
-            touched(i)
-        if not current_touched & pin_bit and last_touched & pin_bit:
-            released(i)
-    last_touched = current_touched
-    time.sleep(0.01) 
+    if not device.begin():
+	sys.exit(1)
+
+    print 'boobs'
+    pygame.mixer.pre_init(44100, -16, 12, 512)
+    pygame.init()
+
+    print 'boobs'
+    sounds = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+    updateSoundArray.lastIns = -1
+
+    print 'boobs'
+    print "yep"
+    sys.stdout.flush()
+    t = threading.Thread(target = runSounds, args=())
+    t.start()
+
+
