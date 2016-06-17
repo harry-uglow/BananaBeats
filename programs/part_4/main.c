a0aa4ec378685919586e4303f39a4608b144da14
datglowtho <harryu@live.co.uk>
2016-06-17 10:03:24 +0100

    Oops. That last change didn't work.

--- programs/part_4/main.c
+++ programs/part_4/main.c
@@ -35,7 +35,7 @@ int main(void) {
 //	if(fscanf(pyPipe, "%d", &test) != 1) {
 //            continue;
 //        }
-        char *inputString;
+        char inputString[9];
         fgets(inputString, 100, pyPipe);
         //fflush(pyPipe);
         if(!inputString || !*inputString || !*(inputString + 5)) {
